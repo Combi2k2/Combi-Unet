abbf6770c5f190af1e5be1426e0a6d8a93391dfc
Combi2k2 <cumbee2k2@gmail.com>
2022-03-05 00:43:58 +0100

    I change the structure of Dataset (before I did not look at the size of input image, it was crazily large)

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,68 +1,39 @@
-from os.path import splitext
-from os import listdir
 import numpy as np
-from glob import glob
 import torch
 from torch.utils.data import Dataset
-from PIL import Image
+import cv2
+import os
 
 
 class BasicDataset(Dataset):
-    def __init__(self, imgs_dir, masks_dir, scale = 1, mask_suffix=''):
-        self.imgs_dir  = imgs_dir
-        self.masks_dir = masks_dir
-        self.scale = scale
-        self.mask_suffix = mask_suffix
-        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+    def __init__(self, imgs_dir, masks_dir):
+        self.images = []
+        self.masks = []
 
-        self.ids = [splitext(file)[0] for file in listdir(imgs_dir)
-                    if not file.startswith('.')]
+        show_size = True
+
+        for file in os.listdir(imgs_dir):
+            file_dir = imgs_dir + '/' + file
+            img  = cv2.imread(file_dir)
+            img  = cv2.resize(img, dsize = (300, 200), interpolation = cv2.INTER_AREA)
+
+            self.images.append(img)
+
+        for file in os.listdir(masks_dir):
+            file_dir = masks_dir + '/' + file
+            mask = cv2.imread(file_dir, cv2.IMREAD_GRAYSCALE)
+            mask = cv2.resize(mask, dsize = (300, 200), interpolation = cv2.INTER_AREA)   
+
+            self.masks.append(mask)
 
     def __len__(self):
-        return len(self.ids)
-
-    @classmethod
-    def preprocess(cls, pil_img, scale):    #nomalize the image
-        w, h = pil_img.size
-        newW, newH = int(scale * w), int(scale * h)
-        assert newW > 0 and newH > 0, 'Scale is too small'
-
-        pil_img = pil_img.resize((newW, newH))
-        img_np  = np.array(pil_img)
-
-        if len(img_np.shape) == 2:
-            img_np = np.expand_dims(img_np, axis = 2)
-
-        # HWC to CHW
-        img_trans = img_np.transpose((2, 0, 1))
-
-        if  img_trans.max() > 1:
-            img_trans = img_trans / 255.
-
-        return img_trans
+        return len(self.images)
 
     def __getitem__(self, i):
-        idx = self.ids[i]
-        mask_file = glob(self.masks_dir + idx + self.mask_suffix + '.*')
-        img_file  = glob(self.imgs_dir + idx + '.*')
+        img  = self.images[i].transpose((2, 0, 1))
+        mask = self.masks[i]
 
-        assert(len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {idx}: {mask_file}')
-        assert(len(img_file)  == 1, f'Either no image or multiple images found for the ID {idx}: {img_file}')
-        
-        mask = Image.open(mask_file[0])
-        img  = Image.open(img_file[0])
+        img  = torch.from_numpy(img).type(torch.FloatTensor)
+        mask = torch.from_numpy(mask).type(torch.FloatTensor)
 
-        assert(img.size == mask.size, f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}')
-
-        img  = self.preprocess(img, self.scale)
-        mask = self.preprocess(mask, self.scale)
-
-        return {
-            'image': torch.from_numpy(img).type(torch.FloatTensor),
-            'mask': torch.from_numpy(mask).type(torch.FloatTensor)
-        }
-
-
-class CarvanaDataset(BasicDataset):
-    def __init__(self, imgs_dir, masks_dir, scale = 1):
-        super().__init__(imgs_dir, masks_dir, scale, mask_suffix = '_mask')
+        return img, mask
